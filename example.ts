5c79538d77dc84034f3a0a5355d41931adb6b504
Ben St John <ben@affinitybridge.com>
2019-01-03 17:51:47 -0800

    WIP

---
+++ example.ts
@@ -0,0 +1,11 @@
+export const puzzles = {
+    word: {
+        id: 'word',
+        threeLetterPermutations: [
+            'rod',
+        ],
+        fourLetterPermutations: [
+            'word',
+        ],
+    }
+};
--- generate_puzzles.py
+++ generate_puzzles.py
@@ -1,11 +1,48 @@
+import csv
 from itertools import permutations
 import enchant
 
-test_word = 'testing'
-three_letter_perms = set([''.join(p) for p in permutations(test_word, 3)])
-dictionary = enchant.Dict('en_US')
 
-for perm in three_letter_perms:
-    if dictionary.check(perm):
-        print(perm)
+def buildJSON():
+    body = '';
+    # with open('words.csv') as csvfile:
+    with open('words_abridged.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            body += buildJSONObjectForWord(''.join(row))
+    header = 'export const puzzles = {\n'
+    footer = '\n});'
+    return header + body + footer
 
+def buildJSONObjectForWord(word):
+    return ('    solution: \'' + word + '\',\n'
+            '    puzzle: \'' + buildPuzzleForWord(word) + '\',\n'
+            '    threeLetterPermutations: ' + buildJSONArrayForWords(buildPermutations(word, 3)) + '\n'
+            '    fourLetterPermutations: ' + buildJSONArrayForWords(buildPermutations(word, 4)) + '\n'
+            '    fiveLetterPermutations: ' + buildJSONArrayForWords(buildPermutations(word, 5)) + '\n'
+            '    sixLetterPermutations: ' + buildJSONArrayForWords(buildPermutations(word, 6)) + '\n')
+
+def buildJSONArrayForWords(words):
+    header = '[\n'
+    body = ''
+    for word in words:
+        body += '        \'' + word + '\',\n'
+    footer = '    ],'
+    return header + body + footer
+
+def buildPuzzleForWord(word):
+    allPermutations = set([''.join(p) for p in permutations(word)])
+    for p in allPermutations:
+        if not isEnglishWord(p):
+            return p
+    return word
+
+def buildPermutations(word, length):
+    allPermutations = set([''.join(p) for p in permutations(word, length)])
+    return [p for p in allPermutations if isEnglishWord(p)]
+
+def isEnglishWord(word):
+    dictionary = enchant.Dict('en_US')
+    return dictionary.check(word)
+
+print(buildJSON())
